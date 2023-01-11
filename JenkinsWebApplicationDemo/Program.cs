cad23dfe8e4eff2f160894b61111f816792f14ba
RITZ\nitin <nitin6312offcieuser@outlook.com>
2023-01-11 08:29:49 +0530

    New changes

--- JenkinsWebApplicationDemo/Program.cs
+++ JenkinsWebApplicationDemo/Program.cs
@@ -12,6 +12,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
+// New comment added
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
